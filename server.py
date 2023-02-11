c99b8adfbbf633feedba9d1547671fa682d43a06
MRajaeiM <mrajaeim.pro@gmail.com>
2023-02-11 18:52:27 +0330

    [fix] resolve initial answer in server

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ from app import predict_answer
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 async def index(request: Request, response: Response, text: str = None):
-    if text:
-        answer = predict_answer(text).replace("AI: ",'')
+    # if text:
+    answer = "" if text is None else predict_answer(text).replace("AI: ", '')
     return templates.TemplateResponse("form_and_answer.html", {"request": request, "text": text, "answer": answer})
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
